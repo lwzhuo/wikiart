54e6283780a43a7363a312e8b5903f81010a3797
lwzhuo <lwzhuo@live.com>
2017-11-15 12:33:00 +0800

    完成多线程代码

--- picinfo-multithreading.py
+++ picinfo-multithreading.py
@@ -4,14 +4,18 @@ from bs4 import BeautifulSoup
 import requests
 import time
 import urllib3
+import threading
+
 urllib3.disable_warnings()
 conn = pymysql.connect(host='localhost',user='root',password='abc123',db='wikiartbackup',charset='utf8')
 cur=conn.cursor()
 headers = {
-        # 'Referer':'https://www.wikiart.org/en/paintings-by-style',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36',
         'Connection': 'Keep-Alive'
     }
+i = 0
+lock = threading.Lock()
+errorPid = []
 
 def getallurl():
     sql = "SELECT id,painturl FROM backupallurl"
@@ -33,11 +37,13 @@ def downloadpage(url):
             if errortime<5:
                 print("***********************************WARNING***********************************")
                 time.sleep(5)
+                errortime+=1
             else:
                 print("***********************************ERROR***********************************")
                 break
         continue
-    if response==None:
+    if response is None:#连接失败 返回None
+        response.close()
         return None
     else:
         response.close()
@@ -87,7 +93,7 @@ def getpicinfo(content):
     attritubeDict['genre'] = genrelist
     attritubeDict['media'] = medialist
     # 组装结束
-    print(attritubeDict)
+    #print(attritubeDict)
     return attritubeDict
 
 def insertinfo(attritubeDict,id):
@@ -97,35 +103,67 @@ def insertinfo(attritubeDict,id):
     styleList = attritubeDict['style']
     mediaList = attritubeDict['media']
     genreList = attritubeDict['genre']
-    info_sql = "INSERT INTO picinfo(id,title,date,arist) " \
-               "VALUES(%s,%s,%s,%s)"%(id,title,date,artist)
+    info_sql = "INSERT INTO picinfo(id,title,date,artist) " \
+               "VALUES(%s,'%s','%s','%s')"%(id,title,date,artist)
     cur.execute(info_sql)
     conn.commit()
     if len(styleList)!=0:
         for style in styleList:
-            style_sql = "INSERT INTO style(id,style) VALUES (%s,%s)"%(id,style)
+            style_sql = "INSERT INTO style(id,style) VALUES ('%s','%s')"%(id,style)
             cur.execute(style_sql)
             conn.commit()
     if len(mediaList)!=0:
         for media in mediaList:
-            media_sql = "INSERT INTO media(id,media) VALUES (%s,%s)"%(id,media)
+            media_sql = "INSERT INTO media(id,media) VALUES ('%s','%s')"%(id,media)
             cur.execute(media_sql)
             conn.commit()
     if len(genreList)!=0:
         for genre in genreList:
-            genre_sql = "INSERT INTO genre(id,genre) VALUES (%s,%s)"%(id,genre)
+            genre_sql = "INSERT INTO genre(id,genre) VALUES ('%s','%s')"%(id,genre)
             cur.execute(genre_sql)
             conn.commit()
 
+class crawlThread(threading.Thread):
+    def __init__(self,Tid,urllist):
+        threading.Thread.__init__(self)
+        self.Tid = Tid
+        self.urllist = urllist
+    def run(self):
+        while True:
+            print("Thread:",self.Tid,"run")
+            global i
+            lock.acquire()
+            flag = i
+            i+=1
+            lock.release()
+            if flag>len(urllist):#结束条件
+                break
+            Pid = self.urllist[flag][0]
+            fullurl = "https://www.wikiart.org" + self.urllist[flag][1]#补全URL
+            htmlpage = downloadpage(fullurl)
+            if htmlpage is None:
+                lock.acquire()
+                print(self.Tid,"ERROR url:",fullurl,"Pid: ",Pid)
+                errorPid.append(Pid)
+                lock.release()
+            else:
+                datadict = getpicinfo(htmlpage)
+                lock.acquire()#读入数据库上锁
+                insertinfo(datadict,Pid)
+                print(self.Tid,"success Pid:",Pid," URL:",fullurl," sleep 5s")
+                lock.release()
+                time.sleep(5)
+
 if __name__ == '__main__':
     urllist = getallurl()
-    print len(urllist)
+    # print(urllist[0][0])
     # url = "https://www.wikiart.org"+urllist[0][1]
     # url = "https://www.wikiart.org/en/mathias-goeritz/torres-de-sat-lite-collaboration-with-luis-barrag-n-and-jes-s-reyes-ferreira"
-    # print url
+    # print(url)
     # page = downloadpage(url)
     # data = getpicinfo(page)
-    # print data
+    # print(data['title'])
+    # print(type(data['title']))
     # print data['media']
     # print len(data['media'])
     # print data['date'] is None
@@ -133,6 +171,19 @@ if __name__ == '__main__':
     #     print style
     # for media in data['media']:
     #     print style
-
+    #空值插入测试开始
+    # id=1
+    # title =None
+    # date = None
+    # artist =None
+    # info_sql = "INSERT INTO picinfo(id,title,date,artist) " \
+    #            "VALUES(%s,'%s','%s','%s')" % (id, title, date, artist)
+    # cur.execute(info_sql)
+    # conn.commit()
+    #空值插入测试结束
+    for i in range(200):
+        t = crawlThread(i,urllist)
+        t.start()
+    t.join()
     cur.close()
     conn.close()
