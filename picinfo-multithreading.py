db25e0a337208c635213e698d93d0725a5a89546
lwzhuo <lwzhuo@live.com>
2017-12-02 22:53:02 +0800

    优化多线程爬虫 完善异常处理

--- picinfo-multithreading.py
+++ picinfo-multithreading.py
@@ -18,7 +18,7 @@ headers = {
     'Host':'www.wikiart.org',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'
     }
-apiUrl = ""
+apiUrl = ""#api接口
 i = 0
 lock = threading.Lock()
 errorPid = []
@@ -36,7 +36,16 @@ def getproxy():
     response.close()
     birthtime = time.time()#获取ip生成时间
     # print(response.status_code)
+    if response.status_code == 429:#测试抓取
+        file = open("PROXYLOG.txt", 'a')
+        file.write("too many requests\n")
+        file.close()
     if response.status_code == 200:
+        if ip[0]=='{':#抓取频率过快 显示错误信息 错误处理
+            file = open("PROXYLOG.txt", 'a')
+            file.write("200 errmsg\n")
+            file.close()
+            return
         proxy = dict()
         proxy["http"] = str(ip, encoding="utf8").strip('\n')
         proxy["https"] = str(ip, encoding="utf8").strip('\n')
@@ -49,7 +58,7 @@ def getproxy():
         file.close()
         lock.release()
 
-def needGetProxy():#获得最后加进列表的ip的时间 计算是否大于6s 大于就加入 比避免频繁申请
+def needGetProxy():#获得最后加进列表的ip的时间 计算是否大于6s 大于就加入 避免频繁申请
     nowtime = time.time()
     birthtime = proxyList[len(proxyList)-1][0]
     if nowtime - birthtime>=6:
@@ -58,13 +67,11 @@ def needGetProxy():#获得最后加进列表的ip的时间 计算是否大于6s 
         return False
 
 def updateProxyList():#清除过时代理
-    lock.acquire()
     nowtime = time.time()
     if len(proxyList)>0:
         for proxy in proxyList:
-            if nowtime - proxy[0]>110:#这批代理生存期120s
+            if nowtime - proxy[0]>280:#这批代理生存期300s
                 proxyList.remove(proxy)
-    lock.release()
 
 def downloadpage(url,Pid):
     sess = requests.session()
@@ -74,32 +81,25 @@ def downloadpage(url,Pid):
     errortime = 0
     while response == "":
         try:
-            updateProxyList()
             lock.acquire()
+            updateProxyList()
             proxy = random.choice(proxyList)#随机抽取一个代理
             proxy = proxy[1]
             lock.release()
             response = sess.get(url=url, headers=headers, proxies=proxy, verify=False, timeout=10)
             # print("Proxy:",proxylist["http"])
         except Exception as e:
-            print(e)
-            if errortime<30:#试错机会
+            if errortime<50:#试错机会
                 lock.acquire()
-                print("***********************************WARNING***********************************%s"%response)
-                # file = open("ERRLOG.txt",'a')
-                # str = "WARNING:%d  %s\n" % (Pid, url)
-                # file.write(str)
-                # file.close()
+                print("******WARNING******",e)#打印错误
+                errortime += 1
                 sess.close()
                 lock.release()
-                # time.sleep(5)
-                # getproxy()
-                errortime+=1
             else:
                 lock.acquire()
                 print("***********************************ERROR***********************************")
                 file = open("ERRLOG.txt",'a')
-                str="ERROR:%d  %s\n" % (Pid, url)
+                str="ERROR:download Exception %d  %s\n" % (Pid, url)
                 file.write(str)
                 file.close()
                 lock.release()
@@ -113,51 +113,54 @@ def downloadpage(url,Pid):
         return response.content
 
 def getpicinfo(content):
-    attritubeDict = {}
-    soup = BeautifulSoup(content, 'lxml')
-    divlist = soup.find_all('div', class_='info')
-    title = divlist[0].find('div', class_='info-line painting-header').find('h1').text  # 标题
-    # artist开始
-    artist = divlist[0].find('div', class_='info-line painting-header').find('a', class_='artist-name').text
-    # artist结束
-    # date开始
-    date = divlist[0].find('span', itemprop='dateCreated')
-    if date is not None:
-        date = date.text
-    # style开始
-    stylelist = []
-    style = divlist[0].find('span', text='Style:')
-    if style is not None:
-        style = style.parent.find_all('a')
-        for s in style:
-            stylelist.append(s.text)
-    # style结束
-    # genre开始
-    genrelist = []
-    genre = divlist[0].find('span', text='Genre:')
-    if genre is not None:
-        genre = genre.parent.find_all('a')
-        for g in genre:
-            genrelist.append(g.text)
-    # genre结束
-    # media开始
-    medialist = []
-    media = divlist[0].find('span', text='Media:')
-    if media is not None:
-        media = media.parent.find_all('a')
-        for m in media:
-            medialist.append(m.text)
-    # media 结束
-    # 组装开始
-    attritubeDict['title'] = title
-    attritubeDict['artist'] = artist
-    attritubeDict['date'] = date
-    attritubeDict['style'] = stylelist
-    attritubeDict['genre'] = genrelist
-    attritubeDict['media'] = medialist
-    # 组装结束
-    #print(attritubeDict)
-    return attritubeDict
+    try:
+        attritubeDict = {}
+        soup = BeautifulSoup(content, 'lxml')
+        divlist = soup.find_all('div', class_='info')
+        title = divlist[0].find('div', class_='info-line painting-header').find('h1').text  # 标题
+        # artist开始
+        artist = divlist[0].find('div', class_='info-line painting-header').find('a', class_='artist-name').text
+        # artist结束
+        # date开始
+        date = divlist[0].find('span', itemprop='dateCreated')
+        if date is not None:
+            date = date.text
+        # style开始
+        stylelist = []
+        style = divlist[0].find('span', text='Style:')
+        if style is not None:
+            style = style.parent.find_all('a')
+            for s in style:
+                stylelist.append(s.text)
+        # style结束
+        # genre开始
+        genrelist = []
+        genre = divlist[0].find('span', text='Genre:')
+        if genre is not None:
+            genre = genre.parent.find_all('a')
+            for g in genre:
+                genrelist.append(g.text)
+        # genre结束
+        # media开始
+        medialist = []
+        media = divlist[0].find('span', text='Media:')
+        if media is not None:
+            media = media.parent.find_all('a')
+            for m in media:
+                medialist.append(m.text)
+        # media 结束
+        # 组装开始
+        attritubeDict['title'] = title
+        attritubeDict['artist'] = artist
+        attritubeDict['date'] = date
+        attritubeDict['style'] = stylelist
+        attritubeDict['genre'] = genrelist
+        attritubeDict['media'] = medialist
+        # 组装结束
+        return attritubeDict
+    except Exception as e:
+        print(e)
+        return None
 
 def insertinfo(attritubeDict,id):
     title = attritubeDict['title']
@@ -166,35 +169,50 @@ def insertinfo(attritubeDict,id):
     styleList = attritubeDict['style']
     mediaList = attritubeDict['media']
     genreList = attritubeDict['genre']
-    #空值判断预处理
-    if title is None:
-        title = "NULL"
-    if date is None:
-        date = "NULL"
-    if artist is None:
-        artist = "NULL"
-    #空值判断预处理结束
-    lock.acquire()
-    info_sql = "INSERT INTO picinfo(id,title,date,artist) " \
-               "VALUES(%s,%s,%s,%s)"
-    cur.execute(info_sql,(id,title,date,artist))
-    conn.commit()
-    if len(styleList)!=0:
-        for style in styleList:
-            style_sql = 'INSERT INTO style(id,style) VALUES ("%s","%s")'%(id,style)
-            cur.execute(style_sql)
-            conn.commit()
-    if len(mediaList)!=0:
-        for media in mediaList:
-            media_sql = 'INSERT INTO media(id,media) VALUES ("%s","%s")'%(id,media)
-            cur.execute(media_sql)
-            conn.commit()
-    if len(genreList)!=0:
-        for genre in genreList:
-            genre_sql = 'INSERT INTO genre(id,genre) VALUES ("%s","%s")'%(id,genre)
-            cur.execute(genre_sql)
-            conn.commit()
-    lock.release()
+    #组装sql语句
+    argc=[]
+    sql1 = "INSERT INTO picinfo(id"
+    sql2 = "VALUES(%s"
+    argc.append(id)
+    if title is not None:
+        sql1+=",title"
+        sql2+=",%s"
+        argc.append(title)
+    if date is not None:
+        sql1 += ",date"
+        sql2 += ",%s"
+        argc.append(date)
+    if artist is not None:
+        sql1 += ",artist"
+        sql2 += ",%s"
+        argc.append(artist)
+    for x in range(len(styleList)):
+        sql1+=",style%d"%(x+1)
+        sql2+=",%s"
+        argc.append(styleList[x])
+    for x in range(len(mediaList)):
+        sql1+=",media%d"%(x+1)
+        sql2+=",%s"
+        argc.append(mediaList[x])
+    for x in range(len(genreList)):
+        sql1+=",genre%d"%(x+1)
+        sql2+=",%s"
+        argc.append(genreList[x])
+    sql1+=")"
+    sql2+=")"
+    sql=sql1+sql2
+    try:
+        lock.acquire()
+        cur.execute(sql,argc)
+        conn.commit()
+        lock.release()
+    except Exception as e:#插入异常处理
+        lock.acquire()
+        file = open("ERRLOG.txt", 'a')
+        str = "ERROR:SQL Exception Pid:%d\n" % (id)
+        file.write(str)
+        file.close()
+        lock.release()
 
 class crawlThread(threading.Thread):
     def __init__(self,Tid,urllist):
@@ -203,13 +221,12 @@ class crawlThread(threading.Thread):
         self.urllist = urllist
     def run(self):
         while True:
-            # if self.Tid%15 == 1:#部分线程执行抓取新的代理的工作
-            #     getproxy()
             lock.acquire()
             flag = needGetProxy()
             lock.release()
             if flag:
-                getproxy()
+                if random.random() > 0.5:  # 限制部分线程参与抓取新ip任务 防止抓取过快
+                    getproxy()
             lock.acquire()
             global i
             flag = i
@@ -221,19 +238,24 @@ class crawlThread(threading.Thread):
             Pid = self.urllist[flag][0]
             fullurl = "https://www.wikiart.org" + self.urllist[flag][1]#补全URL
             htmlpage = downloadpage(fullurl,Pid)
-            if htmlpage is None:
-                # lock.acquire()
+            if htmlpage is None:#处理下载异常
                 print(self.Tid,"ERROR url:",fullurl,"Pid: ",Pid)
-                # errorPid.append(Pid)#加入错误列表
-                # lock.release()
             else:
                 datadict = getpicinfo(htmlpage)
-                insertinfo(datadict,Pid)
-                print("Thread ",self.Tid,"success Pid:",Pid," URL:",fullurl," sleep 5s")
+                if datadict is None:#处理html解析异常
+                    lock.acquire()
+                    file = open("ERRLOG.txt", 'a')
+                    str = "ERROR:parse Exception Pid:%d  URL:%s\n" % (Pid, fullurl)
+                    file.write(str)
+                    file.close()
+                    lock.release()
+                else:
+                    insertinfo(datadict,Pid)
+                    print("Thread ",self.Tid,"success Pid:",Pid," URL:",fullurl)
                 # time.sleep(5)
 
 if __name__ == '__main__':
-    urllist = getallurl()
+
     #测试开始
     # print(urllist[0][0])
     # url = "https://www.wikiart.org"+urllist[0][1]
@@ -258,16 +280,20 @@ if __name__ == '__main__':
     # print (type(info['date']))
     # insertinfo(info,1)
     #空值插入测试结束
+    start = time.time()
+    urllist = getallurl()
     print("初始化代理列表")
-    for i in range(15):
+    for i in range(40):
         getproxy()
         print(i,proxyList[i])
         time.sleep(6)
     print("代理列表初始化结束")
-    for i in range(300):
+    for i in range(100):
         t = crawlThread(i,urllist)
         t.start()
     t.join()
     cur.close()
     conn.close()
     print("爬虫结束")
+    end = time.time()
+    print(end-start)
